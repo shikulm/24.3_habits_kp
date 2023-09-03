40bdf44a0a35bbc8b01924a49871e22682fecb77
shikulm <shikul_m@mail.ru>
2023-09-03 16:33:35 +0400

    add lessons into serialaizers courses

--- my_application_python/24.1_course_drf/courses/serializers.py
+++ my_application_python/24.1_course_drf/courses/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 
 from courses.models import Course, Lesson
 
+class LessonSerialaizer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 class CourseSerialaizer(serializers.ModelSerializer):
     lessons_count = serializers.IntegerField(source='lessons.count', read_only=True)
-
+    lessons = LessonSerialaizer(many=True)
 
     class Meta:
         model = Course
         fields = '__all__'
 
 
-class LessonSerialaizer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Lesson
-        fields = '__all__'
