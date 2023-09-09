1a700afc357354becd620df7b765188245ab569e
shikulm <shikul_m@mail.ru>
2023-09-09 20:13:58 +0400

    add endpoint subscripe

--- my_application_python/24.1_course_drf/courses/urls.py
+++ my_application_python/24.1_course_drf/courses/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from courses.apps import CoursesConfig
 from courses.views import CourseViewSet, LessonCreateAPIView, LessonRetrieveAPIView, LessonListAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView, SubscriptionListAPIView, \
+    SubscriptionRetrieveAPIView, SubscriptionCreateAPIView, SubscriptionDestroyAPIView
 
 app_name = CoursesConfig.name
 
@@ -16,4 +17,9 @@ urlpatterns = [
     path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(),name = 'lesson-update'),
     path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(),name = 'lesson-delete'),
     path('payment/', PaymentListAPIView.as_view(), name='payment-list'),
+
+    path('subscripe/', SubscriptionListAPIView.as_view(), name='subscripe-list'),
+    path('subscripe/<int:pk>/', SubscriptionRetrieveAPIView.as_view(), name='subscripe-one'),
+    path('subscripe/create/', SubscriptionCreateAPIView.as_view(), name='subscripe-create'),
+    path('subscripe/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view(), name='subscripe-delete'),
 ] + router.urls
