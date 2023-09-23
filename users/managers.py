25a1c7d6666fa0bee2fd3e4e201a45641b014efb
shikulm <shikul_m@mail.ru>
2023-09-23 20:24:04 +0400

    config uses app

--- users/managers.py
+++ users/managers.py
@@ -3,23 +3,23 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password):
+    def create_user(self, telegram, password):
 
-        if not email:
-            raise TypeError('Users must have an email address.')
+        if not telegram:
+            raise TypeError('Users must have an telegram.')
 
         if not password:
-            raise TypeError('Users must have a password.')
+            raise TypeError('Users must have a telegram.')
 
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(telegram=telegram)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, telegram, password):
 
-        user = self.create_user(email, password)
+        user = self.create_user(telegram, password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
